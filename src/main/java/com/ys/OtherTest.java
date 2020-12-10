7be20085f317e4917e60513b6ae26756b973bc62
chuang.li@dotcunited.com <a101010>
2020-12-10 18:10:14 +0800

    Revert "error code 4"
    This reverts commit 9b5e0d46

--- src/main/java/com/ys/OtherTest.java
+++ src/main/java/com/ys/OtherTest.java
@@ -25,10 +25,4 @@ public class OtherTest {
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
 }
